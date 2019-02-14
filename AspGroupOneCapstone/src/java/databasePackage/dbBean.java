6e9e0612617ad349bee71aa0b6ded9e2ae0878f9
Jonathan <Jonathananders6@gmail.com>
2019-02-14 17:24:45 -0500

    DataBase Test Page up and running

--- AspGroupOneCapstone/src/java/databasePackage/dbBean.java
+++ AspGroupOneCapstone/src/java/databasePackage/dbBean.java
@@ -1,7 +1,6 @@
 package databasePackage;
 
 import java.io.Serializable;
-
 import javax.faces.bean.ManagedBean;
 import javax.enterprise.context.RequestScoped;
 
@@ -13,10 +12,19 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class dbBean implements Serializable 
 {
-
+    /*
+    I reccomend using a list, or array for DB quesry results
+    Change code here depending on what you choose.
+    This example might help:
+    https://showcase.bootsfaces.net/forms/DataTable.jsf;jsessionid=MVCkGvoidu4GTGDZq7ZAP0EgLbOMTkpFHQR_kl5D.showcase04#unified_ajax_and_javascript_api
+    
+    Add any needed POJO to "databasePackage"
+    */
+    
     private String firstName;
     private String lastName;
     private int docId;
+    
     /**
      * Creates a new instance of dbBean
      */
@@ -31,6 +39,4 @@ public class dbBean implements Serializable
     public void setLastName(String name) {this.lastName = name;}
     public int getDocId() {return docId;}
     public void setDocId(int id) {this.docId = id;}
-    
-    
 }
