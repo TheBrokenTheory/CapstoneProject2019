69b8670a259d7879f3c987ebd7c2b1b11d928f3e
Jonathan <Jonathananders6@gmail.com>
2019-03-17 20:57:00 -0400

    Bean loading twice bug

--- TomcatCapstoneProject/AspGroupOneCapstoneTC/src/main/java/managedBeans/loginBean.java
+++ TomcatCapstoneProject/AspGroupOneCapstoneTC/src/main/java/managedBeans/loginBean.java
@@ -1,133 +1,118 @@
 package managedBeans;
 
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import javax.enterprise.context.RequestScoped;
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 /**
  *
  * @author Jonathan Anders
  */
 @ManagedBean(name = "loginBean")
-@RequestScoped
-public class loginBean {
-    
+@ViewScoped
+public class loginBean implements Serializable {
     //Connection conn = null;
     //Statement readStatement = null;
     //ResultSet resultSet = null;
     //String readTable="SELECT * FROM Doctor;";
     //String results = "";
-    String name="test";
-    String nametwo="test2";
+    private String username = "";
+    private String password = "";
+    
+    
     /**
      * Creates a new instance of loginBean
      * @throws java.sql.SQLException
      */
     public loginBean() throws SQLException
     {
-        System.out.println("test");
     }
 
-    private static Connection getRemoteConnection() throws ClassNotFoundException, SQLException {
-        return null;
-        /*
-        System.out.println("testingfuck");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String dbName = System.getProperty("aspgroupwebproj");
-        String userName = System.getProperty("aspgroup1");
-        String password = System.getProperty("finalaspgroup1");
-        String hostname = System.getProperty("aspgroupwebproj.chxan6yoffks.us-east-2.rds.amazonaws.com");
-        String port = System.getProperty("3303");
-        String jdbcUrl = "jdbc:mysql://" + hostname + ":3303" + "/" + dbName + "?user=" + userName + "&password=" + password;
-        Connection con = DriverManager.getConnection(jdbcUrl);
-        return con;
-        */
-  }
+    private static Connection getRemoteConnection() throws ClassNotFoundException, SQLException
+    {
+        Connection conn = null;
+        try {
+            //System.out.println("Loading driver...");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //System.out.println("Driver loaded!");
+          } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Cannot find the driver in the classpath!", e);
+          }
+        conn = DriverManager.getConnection("jdbc:mysql://aspgroup1@finalaspgroup1.chxan6yoffks.us-east-2.rds.amazonaws.com:3306/aspgroup1?user=aspgroup1&password=finalaspgroup1");
+        return conn;
+    }
+    
     
     public void login() throws SQLException, ClassNotFoundException
     {
-         // Read RDS connection information from the environment
-  String dbName = System.getProperty("aspgroup1");
-  String userName = System.getProperty("aspgroup1");
-  String password = System.getProperty("finalaspgroup1");
-  String hostname = System.getProperty("aspgroup1@finalaspgroup1.chxan6yoffks.us-east-2.rds.amazonaws.com");
-  String port = System.getProperty("3306");
-  String jdbcUrl = "jdbc:mysql://" + hostname + ":3306" + "/" + dbName + "?user=" + userName + "&password=" + password;
-  
-  // Load the JDBC driver
-  try {
-    System.out.println("Loading driver...");
-    Class.forName("com.mysql.cj.jdbc.Driver");
-    System.out.println("Driver loaded!");
-  } catch (ClassNotFoundException e) {
-    throw new RuntimeException("Cannot find the driver in the classpath!", e);
-  }
-
-  Connection conn = null;
-  Statement setupStatement = null;
-  Statement readStatement = null;
-  ResultSet resultSet = null;
-  String results = "";
-  int numresults = 0;
-  String statement = null;
-
-  try {
-    // Create connection to RDS DB instance
-    conn = DriverManager.getConnection("jdbc:mysql://aspgroup1@finalaspgroup1.chxan6yoffks.us-east-2.rds.amazonaws.com:3306/aspgroup1?user=aspgroup1&password=finalaspgroup1");
     
-    // Create a table and write two rows
-    setupStatement = conn.createStatement();
-    String createTable = "CREATE TABLE TestingTwo (Resource char(50));";
-    String insertRow1 = "INSERT INTO TestingTwo (Resource) VALUES ('EC2 Instance');";
-    String insertRow2 = "INSERT INTO TestingTwo (Resource) VALUES ('RDS Instance');";
-    
-    setupStatement.addBatch(createTable);
-    setupStatement.addBatch(insertRow1);
-    setupStatement.addBatch(insertRow2);
-    setupStatement.executeBatch();
-    setupStatement.close();
-    
-  } catch (SQLException ex) {
-    // Handle any errors
-    System.out.println("SQLException: " + ex.getMessage());
-    System.out.println("SQLState: " + ex.getSQLState());
-    System.out.println("VendorError: " + ex.getErrorCode());
-  } finally {
-    System.out.println("Closing the connection.");
-    if (conn != null) try { conn.close(); } catch (SQLException ignore) {}
-  }
+        Statement readStatement = null;
+        ResultSet resultSet = null;
+        String results = "";
+        int numresults = 0;
+        String statement = null;
+        Connection con = getRemoteConnection();
+        
+        try {
+            readStatement = con.createStatement();
+            resultSet = readStatement.executeQuery("SELECT username FROM Users;");
+            
+            ResultSetMetaData metadata = resultSet.getMetaData();
+            int numberOfColumns = metadata.getColumnCount();
+            ArrayList<String> arrayList = new ArrayList<String>();
+            boolean usernameMatch = false;
+            
+            while (resultSet.next() || usernameMatch==true)
+            {
+                results = resultSet.getString("username");
+                if(username.equals(results))
+                {
+                    System.out.println("Match!");
+                    usernameMatch = true;
+                    resultSet.close();
+                    readStatement.close();
+                }
+                else
+                {
+                    System.out.println("No Match");
+                }
+            }
+            
+            
+            
+            //resultSet.first();
+            //results = resultSet.getString("username");
+            //resultSet.next();
+            //results += ", " + resultSet.getString("username");
+            //System.out.println(results);
+            //System.out.println(username + " " + password);
+            resultSet.close();
+            readStatement.close();
+            con.close();
 
-  try {
-    conn = DriverManager.getConnection(jdbcUrl);
-    
-    readStatement = conn.createStatement();
-    resultSet = readStatement.executeQuery("SELECT Resource FROM Beanstalk;");
+          } catch (SQLException ex) {
+            // Handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+          } finally {
+                System.out.println("Closing the connection.");
+                if (con != null) try { con.close(); } catch (SQLException ignore) {}
+          }
 
-    resultSet.first();
-    results = resultSet.getString("Resource");
-    resultSet.next();
-    results += ", " + resultSet.getString("Resource");
-    
-    resultSet.close();
-    readStatement.close();
-    conn.close();
-
-  } catch (SQLException ex) {
-    // Handle any errors
-    System.out.println("SQLException: " + ex.getMessage());
-    System.out.println("SQLState: " + ex.getSQLState());
-    System.out.println("VendorError: " + ex.getErrorCode());
-  } finally {
-       System.out.println("Closing the connection.");
-      if (conn != null) try { conn.close(); } catch (SQLException ignore) {}
-  }
     }
-    public String getName() {return name;}
-    public String getNameTwo() {return nametwo;}
-    public void setName(String name) {this.name = name;}
-    public void setNameTwo(String name) {this.nametwo = name;}
+    
+    
+    //Getters and setters
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
 }
