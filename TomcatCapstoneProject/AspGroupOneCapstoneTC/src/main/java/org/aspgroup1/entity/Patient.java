78272aafd1b02245ec9cf968b20d2b730d64c1ff
t.frankbutler <t.frankbutler@gmail.com>
2019-04-14 00:20:04 -0400

    Update Patient.java

--- TomcatCapstoneProject/AspGroupOneCapstoneTC/src/main/java/org/aspgroup1/entity/Patient.java
+++ TomcatCapstoneProject/AspGroupOneCapstoneTC/src/main/java/org/aspgroup1/entity/Patient.java
@@ -1,14 +1,87 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.aspgroup1.entity;
 
-/**
- *
- * @author tfran
- */
-public class Patient {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Patient")
+public class Patient implements Serializable {
+    
+    @Id
+    @Column(name="patientID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int patientID;
+       
+    @Column(name="patientFirstName")
+    private String patientFirstName;
+       
+    @Column(name="patientLastName")
+    private String patientLastName;
+       
+    @Column(name="patientDOB")
+    private String patientDOB;
+       
+    @Column(name="patientAddress")
+    private String patientAddress;
+       
+    @Column(name="patientPhoneNum")
+    private String patientPhoneNum;
+       
+    @Column(name="patientInsurance")
+    private String patintInsurance;
+
+    public Patient(){
+        
+    }
+       
+    public int getPatientID() {
+        return patientID;
+    }
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+    public String getPatientFirstName() {
+        return patientFirstName;
+    }
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName = patientFirstName;
+    }
+    public String getPatientLastName() {
+        return patientLastName;
+    }
+    public void setPatientLastName(String patientLastName) {
+        this.patientLastName = patientLastName;
+    }
+    public String getPatientDOB() {
+        return patientDOB;
+    }
+    public void setPatientDOB(String patientDOB) {
+        this.patientDOB = patientDOB;
+    }
+    public String getPatientAddress() {
+        return patientAddress;
+    }
+    public void setPatientAddress(String patientAddress) {
+        this.patientAddress = patientAddress;
+    }
+    public String getPatientPhoneNum() {
+        return patientPhoneNum;
+    }
+    public void setPatientPhoneNum(String patientPhoneNum) {
+        this.patientPhoneNum = patientPhoneNum;
+    }
+    public String getPatintInsurance() {
+        return patintInsurance;
+    }
+    public void setPatintInsurance(String patintInsurance) {
+        this.patintInsurance = patintInsurance;
+    }
+       
     
 }
