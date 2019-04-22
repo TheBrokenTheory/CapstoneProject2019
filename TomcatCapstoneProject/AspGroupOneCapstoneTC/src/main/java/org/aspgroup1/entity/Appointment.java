63bcec309f858086251a5b2000f55cebedbf8ae7
t.frankbutler <t.frankbutler@gmail.com>
2019-04-21 21:48:38 -0400

    Appointment Results implementation

--- TomcatCapstoneProject/AspGroupOneCapstoneTC/src/main/java/org/aspgroup1/entity/Appointment.java
+++ TomcatCapstoneProject/AspGroupOneCapstoneTC/src/main/java/org/aspgroup1/entity/Appointment.java
@@ -10,9 +10,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import org.hibernate.annotations.Proxy;
 
 @Entity
 @Table(name="Appointment")
+@Proxy(lazy = false)
 public class Appointment implements Serializable{
     @Id
     @Column(name="appID")
