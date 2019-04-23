4635d967eaffa8d36b3f76230471b9a933e71a86
Jonathan <Jonathananders6@gmail.com>
2019-04-22 21:17:29 -0400

    Create MedicalRecord.java

---
+++ TomcatCapstoneProject/AspGroupOneCapstoneTC/src/main/java/org/aspgroup1/Records/MedicalRecord.java
@@ -0,0 +1,22 @@
+
+package org.aspgroup1.Records;
+
+/**
+ *
+ * @author Jonathan Anders
+ */
+public class MedicalRecord {
+    private long patientID;
+    private String patientFirstName;
+    private String patientLastName;
+    private String doctorLastName;
+    private long doctorID;
+    private String dateOfApt;
+    private String timeOfApt;
+    private String reasonForVist;
+    private String diagnosis;
+    private String treatment;
+    
+    
+    
+}
